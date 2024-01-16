150e3f8a952d8b81ff0f0306951b980758d4012f
Animesh Anand <animeshanand@Animeshs-MacBook-Air.local>
2024-01-16 21:09:19 +0530

    login redirect url setup

--- src/Pages/HomePage/Homepage.tsx
+++ src/Pages/HomePage/Homepage.tsx
@@ -1,5 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 function HomePage() {
-  return <div>This is HomePage</div>;
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // Get the current origin and append the desired path
+    const homepageURL = `${window.location.origin}/Home`;
+    const loginURL = `https://dev-openapi.5paisa.com/WebVendorLogin/VLogin/Index?VendorKey=Cn5p1aBA7YllUrNuHiRTy1igrxOt0cWx&ResponseURL=${homepageURL}`;
+
+    // Redirect to the dynamically generated homepage URL after login
+    window.location.href = loginURL;
+  };
+
+  return (
+    <div>
+      <p>Hii Mujeeb</p>
+      <button onClick={handleLogin}>Login to 5Paisa</button>
+    </div>
+  );
 }
 
 export default HomePage;
